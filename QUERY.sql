b0188bff5611b4287cf483fa3334120464f361e9
GeorgeBlaize <georgepurification@gmail.com>
2026-06-17 20:16:12 +0600

    users added

--- QUERY.sql
+++ QUERY.sql
@@ -1,3 +1,12 @@
 DROP TABLE IF EXISTS Bookings;
 DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Users;
+
+
+CREATE TABLE users (
+    user_id INT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(20)
+);
