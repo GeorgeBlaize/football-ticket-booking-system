6b9a33ae8953e1d4d97d82c63d7899601b70cb21
GeorgeBlaize <georgepurification@gmail.com>
2026-06-17 20:24:57 +0600

    Query 3 added

--- QUERY.sql
+++ QUERY.sql
@@ -84,3 +84,15 @@ SELECT
 FROM users
 WHERE full_name ILIKE 'Tanvir%'
 OR full_name ILIKE '%Haque%';
+
+
+
+-- Query 3
+
+SELECT
+    booking_id,
+    user_id,
+    match_id,
+    COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM bookings
+WHERE payment_status IS NULL;
