97b7716957b267a8f8d088a85da468f6f2ed47bc
GeorgeBlaize <georgepurification@gmail.com>
2026-06-17 20:18:29 +0600

    booking added

--- QUERY.sql
+++ QUERY.sql
@@ -19,3 +19,18 @@ CREATE TABLE matches (
     base_ticket_price DECIMAL(10,2) NOT NULL,
     match_status VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    match_id INT NOT NULL,
+    seat_number VARCHAR(20),
+    payment_status VARCHAR(50),
+    total_cost DECIMAL(10,2) NOT NULL,
+
+    FOREIGN KEY (user_id)
+        REFERENCES users(user_id),
+
+    FOREIGN KEY (match_id)
+        REFERENCES matches(match_id)
+);
